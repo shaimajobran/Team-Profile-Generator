8854c7529c2818b6dabeec51ed3796ca8e92a603
shaimaJobran <shaimajobran222gmail.com>
2021-04-26 15:11:34 -0400

    new files and logic

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,31 @@
+const Intern = require ('../lib/Intern');
+
+//name, email, id, role and school  name  for intern 
+const internTest = new Intern ('Prani', 'prani@gmail.com', 3 , 'Intern', 'UWA')
+
+describe('Intern', () => {
+    it('has a name', () =>{
+        expect(internTest.name).toEqual(expect.any(String))
+        expect(internTest.name.length).toBeGreaterThan(2)
+
+    })
+    it('has an email id', () =>{
+        expect(internTest.email).toEqual(expect.stringContaining('@'))
+    })
+
+    it('has an id that is a number', () => {
+        expect(internTest.id).toEqual(expect.any(Number))
+    })
+
+    it('has a role of engineer', () => {
+        expect(internTest.role).toBe('Intern')
+    })
+
+    it('has a school name', () => {
+        keys = Object.keys(internTest )
+        optionKey = keys[4]
+        expect(optionKey).toBe('school')
+        expect(internTest.school).toEqual(expect.any(String))
+    })
+})
+
